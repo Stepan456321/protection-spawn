f929f425ebc4ef01be67b595ac57ec3d30cc8010
Stepan456321 <130591165+Stepan456321@users.noreply.github.com>
2023-04-13 22:27:05 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
-    private Location spawnLocation;
+    private int spawnX;
+    private int spawnY;
+    private int spawnZ;
     private int protectionRadius;
 
     @Override
@@ -21,22 +23,33 @@ public class Main extends JavaPlugin implements Listener {
 
     private void loadConfig() {
         FileConfiguration config = getConfig();
-        spawnLocation = new Location(
-                Bukkit.getWorld(config.getString("spawn.world")),
-                config.getDouble("spawn.x"),
-                config.getDouble("spawn.y"),
-                config.getDouble("spawn.z")
-        );
+        spawnX = config.getInt("spawn.x");
+        spawnY = config.getInt("spawn.y");
+        spawnZ = config.getInt("spawn.z");
         protectionRadius = config.getInt("protection-radius");
     }
 
     @EventHandler
-    public void onPlayerMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
-        Location playerLocation = player.getLocation();
-        if (playerLocation.distance(spawnLocation) <= protectionRadius) {
-            player.teleport(spawnLocation);
-            player.sendMessage("You cannot leave the spawn area!");
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Location blockLocation = event.getBlock().getLocation();
+        if (isWithinSpawnProtection(blockLocation)) {
+            event.setCancelled(true);
         }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Location blockLocation = event.getBlock().getLocation();
+        if (isWithinSpawnProtection(blockLocation)) {
+            event.setCancelled(true);
+        }
+    }
+
+    private boolean isWithinSpawnProtection(Location location) {
+        int x = location.getBlockX();
+        int y = location.getBlockY();
+        int z = location.getBlockZ();
+        double distance = Math.sqrt(Math.pow(x - spawnX, 2) + Math.pow(y - spawnY, 2) + Math.pow(z - spawnZ, 2));
+        return distance <= protectionRadius;
     }
 }
